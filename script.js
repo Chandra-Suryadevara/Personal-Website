c36a7b0b4254de37dba0b22388ddcfbe77213268
Chandra <chandrashekar.suryad@uleth.ca>
2024-07-02 17:41:17 -0600

    added new images

--- script.js
+++ script.js
@@ -4,3 +4,44 @@ function toggleMenu() {
     menu.classList.toggle("open");
     icon.classList.toggle("open");
   }
+
+  let currentIndex = 0;
+  const projects = document.querySelectorAll('.details-container');
+  const projectsPerPage = 3;
+  
+  function showProjects() {
+    projects.forEach((project, index) => {
+      project.classList.remove('active');
+      if (index >= currentIndex && index < currentIndex + projectsPerPage) {
+        project.classList.add('active');
+      }
+    });
+  }
+  
+  function nextProjects() {
+    if (currentIndex + projectsPerPage < projects.length) {
+      currentIndex += projectsPerPage;
+    }
+    showProjects();
+  }
+  
+  function prevProjects() {
+    if (currentIndex - projectsPerPage >= 0) {
+      currentIndex -= projectsPerPage;
+    }
+    showProjects();
+  }
+  
+
+  
+  // Initialize the display
+  showProjects();
+
+  
+
+
+
+
+
+
+
